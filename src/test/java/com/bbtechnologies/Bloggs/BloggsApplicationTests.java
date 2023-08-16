80828d6105e721226ac344aa7603817328758f64
Banda Brighton <brightonbanda13@gmail.com>
2023-08-16 14:24:48 +0200

    16th august commit

---
+++ src/test/java/com/bbtechnologies/Bloggs/BloggsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbtechnologies.Bloggs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
